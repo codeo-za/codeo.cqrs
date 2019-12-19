5bd9421ece17943857224dfd09084f68b9bc53ab
Davyd McColl <davydm@gmail.com>
2019-12-19 15:28:31 +0200

    :wrench: add Microsoft.Net.Compilers to build @ CI :wrench: allow caching for a full year with no ttl

--- src/Codeo.CQRS/CacheAttribute.cs
+++ src/Codeo.CQRS/CacheAttribute.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.Caching;
 
 namespace Codeo.CQRS
 {
@@ -14,9 +15,17 @@ namespace Codeo.CQRS
     /// </summary>
     public class CacheAttribute : Attribute
     {
+        // this is how many seconds are in a year
+        // https://www.rapidtables.com/calc/time/seconds-in-year.html
+        public const int ONE_YEAR = 31622400;
         public HashSet<string> CacheKeyProperties { get; }
         public CacheExpiration CacheExpiration { get; }
         public int TTL { get; }
+
+        public CacheAttribute(params string[] cacheKeyProperties)
+            : this(ONE_YEAR, cacheKeyProperties)
+        {
+        }
 
         public CacheAttribute(
             int ttl,
